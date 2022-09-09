9e1a5176c38374b69d99bfe89af2256fe952347e
S.M.Jegan <97842714+DOTSOFTWARE2017@users.noreply.github.com>
2022-09-09 18:29:41 +0530

    secound commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mysql=require("mysql2");
 
 const app=express();
 const bodyparser=require('body-parser');
-const { response } = require("express");
+
 
 app.use(bodyparser.json());
 
@@ -50,17 +50,17 @@ app.get("/users",(req,res)=>
 
 // })
  
-// app.get("/jagan",(req,res)=>
-// {
-    
-//     database.query('select * from users Where id=?',[req.query.id],(err,rows,fields)=>
-//     {
-//         if(err)
-//         console.log(err)
-//         else
-//         res.send(rows)
-//     })
-// })
+app.get("/jagan",(req,res)=>
+{
+    console.log(req)
+    database.query('select * from users Where id=?',[req.query.id],(err,rows,fields)=>
+    {
+        if(err)
+        console.log(err)
+        else
+        res.send(rows)
+    })
+})
 
 
 // app.get("/jagan",(req,res)=>
@@ -76,14 +76,48 @@ app.get("/users",(req,res)=>
 // })
 
 
-app.get("/jagan",(req,res)=>
-{
+// app.post("/jagan",(req,res)=>
+// {
     
-    database.query('delete from users Where id=?',[req.query.id],(err,rows,fields)=>
-    {
-        if(err)
-        console.log(err)
-        else
-        res.send(rows)
-    })
+//     database.query('insert into users(id,Name,Age,city) values(?,?,?)',[req.query.id],(err,rows,fields)=>
+//     {
+//         if(err)
+//         console.log(err)
+//         else
+//         res.send(rows)
+        
+//     })
+    
+// })
+// const insert_row=()=>
+// {
+//     const query='insert into users(id ,Name,Age,city) values(?,?,?,?);';
+//     let id=1;
+//     let Name="God_saran";
+//     let Age=24;
+//     let city="karur";
+
+//     database.query(query,[id,Name,Age,city],(err,row)=>
+//     {
+//             if (err) throw err
+//             console.log(row.insert_row)
+//             return row
+//     })
+// }
+
+// app.get("/postvalue",(req,res)=>{
+//     const tempValue = insert_row();
+//     res.send(200,tempValue);
+// })
+
+
+database.connect(function(err){
+ if (err) throw err
+ console.log("connect sucessfully !")
+
+ const sql="INSERT INTO USERS (id,Name,Age,city) VALUES ('5','jegan','20','karur')";
+ database.query(sql,(err,res)=>{
+  if(err) throw err
+  console.log("1 record is inserted !")
+ })
 })
